95a2b3d0373d0bb9e817076b6aa525f54343616f
Osadchenko <olegosadchenko.v@gmail.com>
2022-06-15 11:38:15 +0300

    Form learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgModel, Validators } from '@angular/forms';
+
+
+export class User{
+  constructor(public name: string,
+      public email: string,
+      public phone: string){}
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'formLearn';
+
+  // user: User = new User("", "", "");
+  //   addUser(){
+  //       console.log(this.user);
+  //   }
+  myForm : FormGroup;
+    constructor(){
+        this.myForm = new FormGroup({
+
+            "userName": new FormControl("Tom", Validators.required),
+            "userEmail": new FormControl("", [
+                                Validators.required,
+                                Validators.email
+                            ]),
+            "userPhone": new FormControl("", Validators.pattern("[0-9]{10}"))
+        });
+      }
+
+      submit(){
+          console.log(this.myForm);
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
 
-import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+    declarations: [ AppComponent],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
